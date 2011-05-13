3d03a57bbeb85099de56a1bdc26a181d22c15603
dccmx <dccmx@dccmx.com>
2011-05-13 17:15:01 +0800

    sync

--- test.py
+++ test.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python
 import memcache
 import datetime
+import time
 import sys
 
 if __name__=='__main__':
     mc = memcache.Client(['127.0.0.1:'+sys.argv[1]], debug=0)
-    print datetime.datetime.now()
+    t1 = time.time()
     for i in range(10000):
         mc.set("foo","bar")
         value = mc.get("foo")
         if not value == "bar":
             print "error"
             break;
-    print datetime.datetime.now()
+    t2 = time.time()
+    print t2 - t1
